a717c251984d729f7d6c88392e997577afd108fe
Villi <vilhjalmur17@ru.is>
2018-10-13 18:25:36 +0000

    added support for entering a single number, and delivering that single number back

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,10 @@
 function add(number){
-	return 0;
+
+	if(number == ""){
+		return 0;
+	}
+
+	return parseInt(number);
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,7 @@ const add = require("./calculator");
 it("should return zero on an empty string", () => {
 	expect (add("")).toBe(0);
 })
+
+it("should return the number if a single number is entered", () => {
+	expect (add("4")).toBe(4);
+})
